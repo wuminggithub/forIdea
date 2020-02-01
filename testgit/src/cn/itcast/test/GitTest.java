c5baa64dcab29d2bd394966347d83c95cf7a55c5
wuming <wuming@wuming.cn>
2020-02-01 21:40:38 +0800

    dev分支下添加了一个类

--- testgit/src/cn/itcast/test/GitTest.java
+++ testgit/src/cn/itcast/test/GitTest.java
@@ -18,4 +18,7 @@ public class GitTest {
         System.out.println("666");
 
     }
+
+
+
 }
---
+++ testgit/src/cn/itcast/test/Test2.java
@@ -0,0 +1,8 @@
+package cn.itcast.test;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        System.out.println("dev分支下添加了一个类");
+    }
+}
