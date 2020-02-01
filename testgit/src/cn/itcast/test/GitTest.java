694e2b50fca38b8b181834f41c4d08ebb79c903e
wuming <wuming@wuming.cn>
2020-02-01 21:21:14 +0800

    添加method

--- testgit/src/cn/itcast/test/GitTest.java
+++ testgit/src/cn/itcast/test/GitTest.java
@@ -1,10 +1,20 @@
 package cn.itcast.test;
 
+import java.util.Date;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        method();
+    }
+
+    public static void method(){
+        long time = System.currentTimeMillis();
+        Date date = new Date(time);
+        System.out.println(date);
+
     }
 }
