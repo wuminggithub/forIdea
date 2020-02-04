505e8bc8ba6a01c2b2007d824013fb4fd7ffd55c
wuming <wuming@wuming.cn>
2020-02-04 13:01:39 +0800

    spring boot demo

--- springbootdemo/src/main/java/cn/itcast/controller/HelloController.java
+++ springbootdemo/src/main/java/cn/itcast/controller/HelloController.java
@@ -1,12 +1,21 @@
 package cn.itcast.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+
 @RestController
 public class HelloController {
+    @Autowired
+    private DataSource dataSource;
     @GetMapping("/hello")
     public String hello(){
+        System.out.println("datasource::"+dataSource);
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i);
+        }
         return "hello spring boot";
     }
 }
